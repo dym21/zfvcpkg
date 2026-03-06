4852165f63098420d65a2d4f6b326926ce795b0d
dym21 <dym21@163.com>
2026-03-06 15:20:01 +0800

    修改ports文件

--- ai-registry/ports/libbfio/portfile.cmake
+++ ai-registry/ports/libbfio/portfile.cmake
@@ -9,33 +9,46 @@ vcpkg_extract_source_archive_ex(
 	ARCHIVE ${ARCHIVE}
 )
 
-if (VCPKG_TARGET_IS_WINDOWS)
-	vcpkg_execute_required_process(
-                COMMAND "devenv.exe"
-                "libbfio.sln"
-                /Upgrade
-                WORKING_DIRECTORY ${SOURCE_PATH}/msvscpp
-                LOGNAME upgrade-libbfio-${TARGET_TRIPLET}
-        )
-	file(GLOB_RECURSE project_files ${SOURCE_PATH}/*.sln ${SOURCE_PATH}/*.vcxproj)
-	foreach(file ${project_files})
-		vcpkg_replace_string(${file} Release|Win32 Release|x64)
-		vcpkg_replace_string(${file} VSDebug|Win32 Debug|x64)
-		vcpkg_replace_string(${file} MachineX86 MachineX64)
-	endforeach()
-	vcpkg_install_msbuild(
-		SOURCE_PATH "${SOURCE_PATH}"
-		PROJECT_SUBPATH "msvscpp/libbfio.sln"
-	)
-	file(INSTALL ${SOURCE_PATH}/include/ DESTINATION ${CURRENT_PACKAGES_DIR}/include FILES_MATCHING PATTERN "*.h")
-else()
-	vcpkg_configure_make(
-		SOURCE_PATH "${SOURCE_PATH}"
-		COPY_SOURCE
-	)
-	vcpkg_install_make()
-	vcpkg_fixup_pkgconfig()
-endif()
+ if (VCPKG_TARGET_IS_WINDOWS)
+
+	set(MSBUILD_OPTIONS "")
+	set(MSBUILD_OPTIONS_DEBUG "")
+	set(MSBUILD_OPTIONS_RELEASE "")
+
+	if(VCPKG_CRT_LINKAGE STREQUAL "static")
+		list(APPEND MSBUILD_OPTIONS_RELEASE "/p:RuntimeLibrary=MultiThreaded")
+		list(APPEND MSBUILD_OPTIONS_DEBUG "/p:RuntimeLibrary=MultiThreadedDebug")
+	endif()
+     vcpkg_execute_required_process(
+         COMMAND devenv.exe libbfio.sln /Upgrade
+         WORKING_DIRECTORY ${SOURCE_PATH}/msvscpp
+         LOGNAME upgrade-libbfio-${TARGET_TRIPLET}
+     )
+
+     vcpkg_install_msbuild(
+         SOURCE_PATH "${SOURCE_PATH}"
+         PROJECT_SUBPATH "msvscpp/libbfio.sln"
+		 RELEASE_CONFIGURATION "Release"
+		 DEBUG_CONFIGURATION "vsdebug"
+		 OPTIONS ${MSBUILD_OPTIONS}
+		 OPTIONS_RELEASE ${MSBUILD_OPTIONS_RELEASE}
+		 OPTIONS_DEBUG ${MSBUILD_OPTIONS_DEBUG}
+     )
+
+     file(INSTALL
+         ${SOURCE_PATH}/include/
+         DESTINATION ${CURRENT_PACKAGES_DIR}/include
+         FILES_MATCHING PATTERN "*.h"
+     )
+
+ else()
+	 vcpkg_configure_make(
+		 SOURCE_PATH "${SOURCE_PATH}"
+		 COPY_SOURCE
+	 )
+	 vcpkg_install_make()
+	 vcpkg_fixup_pkgconfig()
+ endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(INSTALL "${SOURCE_PATH}/COPYING.LESSER" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
